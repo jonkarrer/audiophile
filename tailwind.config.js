b05311afa6d815e1384fd8d655f8030585e4bdb0
jonkarrer <karrerjon@gmail.com>
2021-10-10 10:42:49 -0500

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,26 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      "xs": "13px",
+      "sm": "14px",
+      "base": "15px",
+      "lg": "18px",
+      "xl": "24px",
+      "2xl": "28px",
+      "3xl": "32px",
+      "4xl": "36px",
+      "5xl": "40px",
+      "6xl": "56px",
+    },
     extend: {
       colors: {
-        "theme-orange": "#FF5A34",
-        splash_button: "#BEBEBE",
-        beige: "#EEE3D0",
-        brown: "#5E464A",
-        teal: "#95CFBD",
-        "theme-pink": "#F093BD",
+        D87D4A: "#D87D4A",
+        FBAF85: "#FBAF85",
+        101010: "#101010",
+        F1F1F1: "#F1F1F1",
+        FAFAFA: "#FAFAFA",
+        FFFFFF: "#FFFFFF",
       },
     },
   },
