70992dca0d3edb2f9fa4e352313b2aae8288c63e
jonkarrer <karrerjon@gmail.com>
2021-10-10 10:54:28 -0500

    removed index from layout

--- components/Layout/index.tsx
+++
@@ -1,7 +0,0 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import { Children } from "../../utils/interfaces";
-
-const Layout = ({ children }: Children) => <div>{children}</div>;
-
-export default Layout;
